45908c49dfcbba0de50514582d799011cdcb7055
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-18 18:18:09 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,7 +2,7 @@ def display_rainbow(array)
   new_array = []
 array.each do |color|
   capital = color[0,1].upcase
-  new_array << "#{capital}: #{color}"
+new_array << "#{capital}: #{color}"
 end
 puts new_array
 end
